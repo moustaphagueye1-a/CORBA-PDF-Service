f52bf4b2d6a1e9d792b2c4413c8058da38616ac8
moustaphagueye1-a <moustapha.gueye1@etu.ussein.edu.sn>
2026-05-09 19:56:36 +0000

    Fix: gunicorn avant Java, port Render auto

--- render-deploy/render-single/start.sh
+++ render-deploy/render-single/start.sh
@@ -1,6 +1,5 @@
 ﻿#!/bin/sh
-DJANGO_PORT="${PORT:-8000}"
 cd /app
 python manage.py migrate --noinput
 java -Xms64m -Xmx200m -Djava.awt.headless=true -Dcom.sun.CORBA.ORBServerHost=localhost -Dcom.sun.CORBA.ORBServerPort=1050 -jar /app/server.jar &
-exec gunicorn config.wsgi:application --bind "0.0.0.0:$DJANGO_PORT" --workers 1 --timeout 120
+exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-10000} --workers 1 --timeout 120
