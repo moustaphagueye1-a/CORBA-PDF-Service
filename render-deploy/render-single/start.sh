f2a2e571435efe974b3b280aaa7aec65531460a7
moustaphagueye1-a <moustapha.gueye1@etu.ussein.edu.sn>
2026-05-09 19:11:10 +0000

    Fix: gunicorn avant Java

--- render-deploy/render-single/start.sh
+++ render-deploy/render-single/start.sh
@@ -1,10 +1,6 @@
 ﻿#!/bin/sh
 DJANGO_PORT="${PORT:-8000}"
-echo "==> Démarrage Java..."
+cd /app
+python manage.py migrate --noinput
 java -Xms64m -Xmx200m -Djava.awt.headless=true -Dcom.sun.CORBA.ORBServerHost=localhost -Dcom.sun.CORBA.ORBServerPort=1050 -jar /app/server.jar &
-echo "==> Java lancé, attente 20s..."
-sleep 20
-echo "==> Migrations Django..."
-cd /app && python manage.py migrate --noinput
-echo "==> Démarrage Gunicorn sur port $DJANGO_PORT..."
-exec gunicorn config.wsgi:application --bind "0.0.0.0:$DJANGO_PORT" --workers 2 --timeout 120
+exec gunicorn config.wsgi:application --bind "0.0.0.0:$DJANGO_PORT" --workers 1 --timeout 120
