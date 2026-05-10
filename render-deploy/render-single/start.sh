eec5e9fc946285b33ad59c04ecd3b05160bb4240
moustaphagueye1-a <moustapha.gueye1@etu.ussein.edu.sn>
2026-05-10 00:51:07 +0000

    start.sh sans boucle attente

--- render-deploy/render-single/start.sh
+++ render-deploy/render-single/start.sh
@@ -1,5 +1,4 @@
 ﻿#!/bin/sh
-cd /app
-python manage.py migrate --noinput
+cd /app && python manage.py migrate --noinput
 java -Xms64m -Xmx200m -Djava.awt.headless=true -Dcom.sun.CORBA.ORBServerHost=localhost -Dcom.sun.CORBA.ORBServerPort=1050 -jar /app/server.jar &
 exec gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-10000} --workers 1 --timeout 120
